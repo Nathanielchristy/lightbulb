0c2704b6487133eb3949d68f402da515b32fd1be
Nathaniel Christy <80506862+Nathanielchristy@users.noreply.github.com>
2022-01-23 22:48:45 +0530

    Master

---
+++ src/components/light.js
@@ -0,0 +1,52 @@
+
+import React, { useState } from "react";
+import "./light.css";
+
+function LightingBulb() {
+  let [toggledOne, setToggleOne] = useState(false);
+  let [toggledTwo, setToggleTwo] = useState(false);
+  const [colorOne, setColorOne] = useState(false);
+  const [colorTwo, setColorTwo] = useState(false);
+  let [lightOne, setLightOne] = useState(false);
+  let [lightTwo, setLightTwo] = useState(false);
+ 
+
+  const toggleOne = function () {
+    if (colorOne) {
+      setColorOne(false);
+      setToggleOne(!toggledOne);
+    } else {
+      setColorOne(true);
+      setLightOne(!lightOne);
+      setToggleOne(!toggledOne);
+    }
+  };
+
+  const toggleTwo = function () {
+    if (colorTwo) {
+      setColorTwo(false);
+      setToggleTwo(!toggledTwo);
+    } else {
+      setColorTwo(true);
+      setLightTwo(!lightTwo);
+      setToggleTwo(!toggledTwo);
+    }
+  };
+
+  return (
+    <div>
+      <div className="board">
+        <div className="column">
+          <div className="bulbOne" style={{ backgroundColor: colorOne ? "orange" : "gray" }} ></div>
+          <button className="switchone" onClick={toggleOne}>{toggledOne ? "ON" : "OFF"}  </button>
+          </div>
+          <div className="column">
+          <div className="bulbTwo" style={{ backgroundColor: colorTwo ? "orange" : "gray" }}></div>
+          <button className="switchtwo" onClick={toggleTwo}>{toggledTwo ? "ON" : "OFF"}  </button>       
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default LightingBulb;
